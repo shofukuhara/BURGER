c691acc5eb89ea77c11cd00154e4f10f7304e79f
shofukuhara <sho.fukuhara0213@gmail.com>
2022-08-13 22:48:16 +0900

    fix:spスタイル調整

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,20 @@ const hamburger = document.querySelector(".header__hamburger");
 const inner = document.querySelector(".header__inner");
 const list = document.querySelector(".header__list");
 const sns = document.querySelector(".header__sns");
+const link = document.querySelectorAll(".header__list-link");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   list.classList.toggle("active");
   sns.classList.toggle("active");
   inner.classList.toggle("active");
+});
+
+link.forEach((link) => {
+  link.addEventListener("click", () => {
+    inner.classList.remove("active");
+    list.classList.remove("active");
+    hamburger.classList.remove("active");
+    sns.classList.remove("active");
+  });
 });
