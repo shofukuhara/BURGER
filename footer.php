fcdf3b253e560ebc8981edb10e4e98188e4ace6f
shofukuhara <sho.fukuhara0213@gmail.com>
2022-08-24 21:34:39 +0900

    fix:jsファイル修正

--- footer.php
+++ footer.php
@@ -4,9 +4,5 @@
 <?php wp_footer(); ?>
 </div>
 </body>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;500;700&display=swap" rel="stylesheet">
-<script src="../js/main.js"> </script>
 
 </html>
--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,27 @@
-const hamburger = document.querySelector(".header__hamburger");
-const inner = document.querySelector(".header__inner");
-const list = document.querySelector(".header__list");
-const sns = document.querySelector(".header__sns");
-const link = document.querySelectorAll(".header__list-link");
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const hamburger = document.querySelector(".header__hamburger");
+    const inner = document.querySelector(".header__inner");
+    const list = document.querySelector(".header__list");
+    const sns = document.querySelector(".header__sns");
+    const link = document.querySelectorAll(".header__list-link");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  list.classList.toggle("active");
-  sns.classList.toggle("active");
-  inner.classList.toggle("active");
-});
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      list.classList.toggle("active");
+      sns.classList.toggle("active");
+      inner.classList.toggle("active");
+    });
 
-link.forEach((link) => {
-  link.addEventListener("click", () => {
-    inner.classList.remove("active");
-    list.classList.remove("active");
-    hamburger.classList.remove("active");
-    sns.classList.remove("active");
-  });
-});
+    link.forEach((link) => {
+      link.addEventListener("click", () => {
+        inner.classList.remove("active");
+        list.classList.remove("active");
+        hamburger.classList.remove("active");
+        sns.classList.remove("active");
+      });
+    });
+  },
+  false
+);
